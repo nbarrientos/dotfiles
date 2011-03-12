f8dd7ad835c3c3dd929633cab5b7d465057bc70c
Nacho Barrientos <nacho@criptonita.com>
2011-03-12 14:24:37 +0100

    Revert "Assign names to tags 8 and 9"
    This reverts commit fd6ae3350f6d5efcbdca816e4270ef278fe07372.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -52,7 +52,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, "mail", "irc" }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
 end
 -- }}}
 
