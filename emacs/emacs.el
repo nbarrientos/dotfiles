02290b03d24289d47222cc83622f971fdd4a0abf
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-03-07 13:45:57 +0100

    Move all my functions to the end of the init file

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -216,23 +216,6 @@
   (magit-clone-url-format "https://%h/%n.git")
   (magit-clone-set-remote.pushDefault t))
 
-(defun my/clone-module (module-name)
-  "Clone a Puppet module from gitlab.cern.ch/ai"
-  (interactive "sModule name: ")
-  (magit-clone-internal
-   ;; Using an internal here, see  https://github.com/magit/magit/discussions/4335
-   (magit-clone--name-to-url (concat "it-puppet-module-" module-name))
-   magit-clone-default-directory
-   nil))
-
-(defun my/clone-hostgroup (hostgroup-name)
-  "Clone a Puppet top-level hostgroup from gitlab.cern.ch/ai"
-  (interactive "sTop-level hostgroup name: ")
-  (magit-clone-internal
-   (magit-clone--name-to-url (concat "it-puppet-hostgroup-" hostgroup-name))
-   magit-clone-default-directory
-   nil))
-
 (use-package ace-window
   :init (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   :bind (("M-o" . ace-window)))
@@ -408,3 +391,20 @@
     (progn
       (copy-file filename (concat "~/afs/www/bucket/" new-filename) t)
       (kill-new (concat "https://cern.ch/nacho/bucket/" new-filename)))))
+
+(defun my/clone-module (module-name)
+  "Clone a Puppet module from gitlab.cern.ch/ai"
+  (interactive "sModule name: ")
+  (magit-clone-internal
+   ;; Using an internal here, see  https://github.com/magit/magit/discussions/4335
+   (magit-clone--name-to-url (concat "it-puppet-module-" module-name))
+   magit-clone-default-directory
+   nil))
+
+(defun my/clone-hostgroup (hostgroup-name)
+  "Clone a Puppet top-level hostgroup from gitlab.cern.ch/ai"
+  (interactive "sTop-level hostgroup name: ")
+  (magit-clone-internal
+   (magit-clone--name-to-url (concat "it-puppet-hostgroup-" hostgroup-name))
+   magit-clone-default-directory
+   nil))
