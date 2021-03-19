d553f6806f42b82d5ff8da17da2b6eb97080c5d3
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-03-19 09:57:24 +0100

    Always apply the marks without asking when leaving the hdrs view

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -354,6 +354,7 @@ the previously multi-windowed one"
   (mu4e-view-show-addresses t)
   (mu4e-compose-dont-reply-to-self t)
   (mu4e-hide-index-messages t)
+  (mu4e-headers-leave-behavior 'apply)
   (mu4e-headers-date-format "%d/%m/%y")
   (mu4e-maildir "~/Mail")
   (mu4e-drafts-folder "/cern/Drafts")
