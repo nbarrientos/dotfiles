3bb7a96ab0983362b2a44b230c4f71f34d487446
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-03-19 11:35:32 +0100

    Let's try doom-themes' dracula for a while

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -150,7 +150,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-moonlight t)
+  (load-theme 'doom-dracula t)
   (doom-themes-visual-bell-config))
 
 (use-package doom-modeline
