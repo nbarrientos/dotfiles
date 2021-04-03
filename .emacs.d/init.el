9fcb892f07ad9ec767d3e603f942d351f786d5c0
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-04-03 10:59:43 +0200

    Correct hook names

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -303,8 +303,8 @@
 (use-package enh-ruby-mode
   :mode (("\\.rb\\'" . enh-ruby-mode)))
 (use-package inf-ruby
-  :hook ((ruby-mode-hook . inf-ruby-minor-mode)
-         (compilation-filter-hook . inf-ruby-minor-mode)))
+  :hook ((ruby-mode . inf-ruby-minor-mode)
+         (compilation-filter . inf-ruby-minor-mode)))
 (use-package puppet-mode)
 (use-package rspec-mode)
 (use-package rubocop
