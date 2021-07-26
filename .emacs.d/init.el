7dccca27af207a62964b27e2de70f459fa8932b7
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-07-26 22:44:51 +0200

    Show only hour and minute in 24h format in the headers view

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -735,6 +735,7 @@ the previously multi-windowed one"
   (mu4e-split-view nil)
   (mu4e-view-html-plaintext-ratio-heuristic most-positive-fixnum)
   (mu4e-headers-date-format "%d/%m/%y")
+  (mu4e-headers-time-format "%R")
   (mu4e-maildir "~/Mail")
   (mu4e-drafts-folder "/cern/Drafts")
   (mu4e-sent-folder   "/cern/Sent Items")
