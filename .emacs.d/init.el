265671845a5a463250fa28a0e05a97a585c4178e
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-12-10 10:32:17 +0100

    Allow back-and-forth in dynamic buffer bookmarks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1518,7 +1518,7 @@ remaps `last-command-event' to the current buffer."
           (assq-delete-all last-command-event my/bm-key-alist)))
   (let ((buffer (cdr (assq last-command-event my/bm-key-alist))))
     (if buffer
-        (switch-to-buffer buffer)
+        (my/switch-to-buffer-if-exists-back-and-forth (buffer-name buffer))
       (add-to-list 'my/bm-key-alist
                    (cons last-command-event (current-buffer)))
       (message (format "Added %s as shortcut for buffer <%s>"
