01815a229d9e9ccfb365c5ff8b4c3dd55522c2f4
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-12-18 19:31:34 +0100

    Redefine counsel-fzf-action only when counsel is loaded
    If project.el is loaded by use-package before counsel then the
    function will be reverted back to the default.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -191,11 +191,6 @@
   (setenv
    "FZF_DEFAULT_COMMAND"
    "find -type f -not -path '*/\.git/*' -not -path '*/spec/fixtures/*' -printf '%P\n'")
-  (defun counsel-fzf-action (x)
-    (with-ivy-window
-      (let ((default-directory counsel--fzf-dir))
-        (find-file-other-window x)))
-    (other-window -1))
   (defun my/project-counsel-fzf ()
     (interactive)
     (let* ((default-directory (project-root (project-current t))))
@@ -520,6 +515,11 @@ modify parts of the directory before switching to it."
   (ivy-set-actions
    'counsel-esh-dir-history
    '(("e" counsel--esh-dir-history-action-edit "edit")))
+  (defun counsel-fzf-action (x)
+    (with-ivy-window
+      (let ((default-directory counsel--fzf-dir))
+        (find-file-other-window x)))
+    (other-window -1))
   :custom
   (counsel-yank-pop-separator "\n-------------------\n")
   (counsel-describe-function-function #'helpful-callable)
