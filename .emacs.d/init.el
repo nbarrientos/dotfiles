230acd9982c86261973e02470281b84e86de62b7
Nacho Barrientos <nacho.barrientos@cern.ch>
2022-05-03 10:54:05 +0200

    Use a simpler face

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1689,7 +1689,7 @@ selection."
                      (+ 1 (point)))))))
             (conf-mode)
             (local-set-key (kbd "q") 'kill-this-buffer)))
-    (message "%s" (propertize "Unknown account!" 'face 'alert-high-face)))))
+    (message "%s" (propertize "Unknown account!" 'face 'warning)))))
 
 (defun my/cern-ldap-group-dwim (arg)
   "Expand the group which is in the active region or the word at point.
@@ -1721,7 +1721,7 @@ With any prefix argument, make it not recursive."
             (local-set-key (kbd "q") 'kill-this-buffer)
             (local-set-key (kbd "C-<return>") 'my/cern-ldap-user-dwim)
             (sort-lines nil (point-min) (point-max))))
-      (message "%s" (propertize "Empty or unknown group!" 'face 'alert-high-face)))))
+      (message "%s" (propertize "Empty or unknown group!" 'face 'warning)))))
 
 (defun my/cern-ldap-group-expand (group &optional recurse)
   "Return (recursively if RECURSE) the members of GROUP."
