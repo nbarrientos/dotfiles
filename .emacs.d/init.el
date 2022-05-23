22b4676e0283353cb03e971ccee2b0af5492b708
Nacho Barrientos <nacho.barrientos@cern.ch>
2022-05-23 19:30:33 +0200

    Use more semantic bindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1800,11 +1800,11 @@ otherwise it returns nil."
   (transient-define-prefix my/cern-dispatch ()
     "Dispatch a CERN-specific command."
     [["LDAP user"
-      ("U" "Dwim" my/cern-ldap-user-dwim)
-      ("u" "Ask" my/cern-ldap-user)]
+      ("u d" "Dwim" my/cern-ldap-user-dwim)
+      ("u l" "Ask" my/cern-ldap-user)]
      ["LDAP group"
-      ("G" "Dwim" my/cern-ldap-group-dwim)
-      ("g" "Ask" my/cern-ldap-group)]]))
+      ("g d" "Dwim" my/cern-ldap-group-dwim)
+      ("g l" "Ask" my/cern-ldap-group)]]))
 
 ;;; IRC
 (use-package erc
