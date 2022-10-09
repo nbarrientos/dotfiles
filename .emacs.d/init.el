7755ada5ee5627e7bd32d45e86a54b222083302f
Nacho Barrientos <nacho.barrientos@cern.ch>
2022-10-09 16:39:05 +0200

    Remove orgmode ELPA repository
    Built-in Emacs or GNU ELPA, this repository only contains <= 9.5.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -83,7 +83,6 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
