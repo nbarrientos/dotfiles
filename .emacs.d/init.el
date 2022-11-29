29824105894565ff34f1400d32e4b6f39125ec82
Nacho Barrientos <nacho.barrientos@cern.ch>
2022-11-29 20:37:33 +0100

    Do not underline links

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -665,7 +665,7 @@ modify parts of the directory before switching to it."
   (doom-themes-visual-bell-config)
   (custom-set-faces
    '(ivy-modified-buffer ((t (:inherit default :foreground unspecified))))
-   '(link ((t (:weight unspecified)))))
+   '(link ((t (:weight unspecified :underline unspecified)))))
   :custom
   (doom-themes-enable-bold t)
   (doom-themes-enable-italic t))
