81a6c1cb09e0a1247452642d9c3b3d3ec0d28a40
Nacho Barrientos <nacho.barrientos@cern.ch>
2023-01-22 11:15:02 +0100

    Add function my/git-link-branch
    Unfortunately `git-link' does not allow temporarily overriding
    `git-link-use-commit' using a prefix argument. I like that by default
    the link is to the commit hash but sometimes I need a way to point to
    the current branch instead.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1105,7 +1105,14 @@ If no universal argument is passed, assume only one output"
 
 (use-package git-link
   :custom
-  (git-link-use-commit t))
+  (git-link-use-commit t)
+  :config
+  (defun my/git-link-branch ()
+    "Create a URL representing the current buffer's location always
+using the branch name."
+    (interactive)
+    (let ((git-link-use-commit nil))
+      (call-interactively #'git-link))))
 
 (use-package ediff-init
   :ensure nil
