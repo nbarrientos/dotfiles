1dab2232c9ca17ba32cfa5bf778e9d4e1e5c0249
Nacho Barrientos <nacho.barrientos@cern.ch>
2023-05-08 10:33:55 +0200

    Show --strategy-option in level 1 (magit-merge)

--- .emacs.d/transient/levels.el
+++ .emacs.d/transient/levels.el
@@ -3,4 +3,6 @@
  (magit-log
   (transient:magit-log:--regexp-ignore-case . 2))
  (magit-log-refresh
-  (transient:magit-log-refresh:--regexp-ignore-case . 2)))
+  (transient:magit-log-refresh:--regexp-ignore-case . 2))
+ (magit-merge
+  (magit-merge:--strategy-option . 1)))
