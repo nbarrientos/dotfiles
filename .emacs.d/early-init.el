2f3218d7b52cd77bb171507e42f4124ebc2de7e5
Nacho Barrientos <nacho.barrientos@cern.ch>
2026-09-06 21:04:11 +0200

    Defer garbage collection during startup; restored once Emacs is up.

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,2 +1,8 @@
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 32 1024 1024)
+                  gc-cons-percentage 0.1)))
 (when (file-exists-p "~/.emacs.d/monkeys.el")
   (load-file "~/.emacs.d/monkeys.el"))
